17df683a1935ddd00aac7f18f1862c24481d2b47
Kayla Cross <kac288@georgetown.edu>
2018-02-26 00:11:55 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,7 +15,7 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  for each do |cheese|
+  array.each do |cheese|
     return cheese if array.find(cheese)
   end
 end
